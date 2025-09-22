477e5f06326c8f231bd687d1b67a0f3048190d83
tahseenx-alt <tahseenanwer100@gmail.com>
2025-09-22 11:24:40 +0000

    Data Types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  // we are using nodejs not browser , so it give error
+
+// number => 2 to power 53
+//bigint
+// string =>""
+//boolean => true or false
+//null => standout value
+//undefined => value is not assigned
+//symbol => unique 
+
+//object 
+
+console.log(typeof null);
