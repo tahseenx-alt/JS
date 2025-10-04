d8a71da572c919a47e3fa129508a03b9279cf431
tahseenx-alt <tahseenanwer100@gmail.com>
2025-10-04 16:57:13 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+
+// switch
+//synatx
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month ="march";
+switch (month) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    
+    default:
+        console.log("wrong month enter");
+        break;
+}
+
+
