8ae42451d710b478730dedcdb5c1dce25f3bb558
tahseenx-alt <tahseenanwer100@gmail.com>
2025-10-05 04:38:42 +0000

    while and do while loop basic

---
+++ 05_iterator/while_and_dowhile.js
@@ -0,0 +1,26 @@
+
+//while  
+
+let index = 0;
+while (index<=10) {
+    console.log(`value pf index is ${index}`);
+    index = index +2;
+}
+
+
+let myarray = ['flash','batman','superman']
+let i =0
+while (i<myarray.length) {
+    console.log(`value is ${myarray[i]}`);
+    i = i+1;
+}
+
+
+//do while loop
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10);
